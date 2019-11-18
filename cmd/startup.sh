d5e5160ef7223a41d2bec584d5f99e37e9f43b1a
“stefan.gu” <http://115.29.47.72:8082/eager_wang/Acmedcare-EMSS-Source.git>
2019-11-18 17:32:44 +0800

    feature(master):  change start.sh

--- cmd/startup.sh
+++ cmd/startup.sh
@@ -5,7 +5,7 @@ fi
 if [ ! -f "/go/logs/start.log" ]; then
   touch "/go/logs/start.log"
 fi
-nohup ./kernel>/go/logs/start.log 2>&1 &
+nohup /go/kernel>/go/logs/start.log 2>&1 &
 echo "kernel is running,you can check /go/logs/start.log"
 sleep 10s
 tail -f ${ServiceRoot}/logs/start.log
